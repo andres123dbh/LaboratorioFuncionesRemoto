f2e61716723f773b978a99973f3184b47653cc87
andres123dbh <60190072+andres123dbh@users.noreply.github.com>
2020-04-04 19:29:51 -0500

    Elever hasta a=0

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-def a_power_b(a,b)
-    for 1 in range(1,b+1):
-        resultado=resultado*resultado
+def funcion(a,b):
+    resultado = a
+    for i in range(1,b):
+        resultado=resultado*a
     return resultado
-
-a=float(input("Ingresar el numeroa elevar"))
-b=float(input("Ingresar el numeroa por el que se elevara"))
-resultado=a_power_b(a,b)
+a=int(input("Ingresar el numeroa elevar: "))
+b=int(input("Ingresar el numeroa por el que se elevara: "))
+resultado=funcion(a,b)
 print(resultado)
+while a != 0:
+    a=int(input("Ingresar el numeroa elevar: "))
+    b=int(input("Ingresar el numeroa por el que se elevara: "))
+    resultado=funcion(a,b)
+    print(resultado)
