95a09e4f783e4b85fa00fdc7a88958f362f24101
andres123dbh <60190072+andres123dbh@users.noreply.github.com>
2020-04-04 20:29:14 -0500

    Ejercicio de numeros perfectos

---
+++ test.py
@@ -0,0 +1,14 @@
+def perfect_number(numero):
+    comparado=0
+    for i in range(1,numero):
+        if numero%i==0:
+            comparado=comparado+i
+            print(comparado)
+            print(i)
+    return comparado
+
+num=int(input("Ingresar numero para evaluar: "))
+comparar=perfect_number(num)
+if num==comparar:
+    print("Es un numero perfecto")
+
